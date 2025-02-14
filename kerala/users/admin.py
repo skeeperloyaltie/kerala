f7465b362f3c40bc1e1218250fb746eeb29b4ca1
skeeperloyaltie <gugod254@gmail.com>
2025-02-14 07:54:43 +0300

    updated admin user view for users

--- kerala/users/admin.py
+++ kerala/users/admin.py
@@ -4,13 +4,13 @@ from .models import User, Receptionist, Doctor
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'user_type', 'is_active')
+    list_display = ('username', 'first_name','last_name', 'user_type', 'is_active')
     search_fields = ('username', 'email')
 
 @admin.register(Receptionist)
 class ReceptionistAdmin(admin.ModelAdmin):
-    list_display = ('user', 'contact_number', 'email')
+    list_display = ('user', 'first_name','last_name', 'contact_number', 'email')
 
 @admin.register(Doctor)
 class DoctorAdmin(admin.ModelAdmin):
-    list_display = ('user', 'specialization', 'contact_number', 'email')
+    list_display = ('user', 'first_name','last_name',  'specialization', 'contact_number', 'email')
