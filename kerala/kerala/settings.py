ee840d37a9feecc476c1b537d4e85fde995e8fe8
skeeperloyaltie <gugod254@gmail.com>
2025-02-17 19:00:41 +0300

    something - nneeeds to e done if this doesnt really allow admins to login

--- kerala/kerala/settings.py
+++ kerala/kerala/settings.py
@@ -27,23 +27,23 @@ import os
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'default-insecure-key')
 DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'True'
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost',  # Example: React frontend during development
-    'http://127.0.0.1:8000',
-    'https://104.37.187.187:8000',
-    "http://localhost:3000",
-    "http://127.0.0.1:8000",
-    "http://smarthospitalmaintain:8000"
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost',  # Example: React frontend during development
+#     'http://127.0.0.1:8000',
+#     'https://104.37.187.187:8000',
+#     "http://localhost:3000",
+#     "http://127.0.0.1:8000",
+#     "http://smarthospitalmaintain:8000"
+# ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 
 
 from corsheaders.defaults import default_headers
 
-CORS_ALLOW_HEADERS = list(default_headers) + [
-    'Username',  # Add the 'Username' header here
-]
 
-CORS_ALLOW_ALL_ORIGINS = DEBUG  # Allow all origins only when DEBUG is True
+# CORS_ALLOW_ALL_ORIGINS = DEBUG  # Allow all origins only when DEBUG is True
 
 ALLOWED_HOSTS = ['smarthospitalmaintain.com', 'www.smarthospitalmaintain.com', '104.37.187,187', '*']
 
