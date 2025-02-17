5647abbd30fa62198d5a480c812cc0fb2f210a1c
skeeperloyaltie <gugod254@gmail.com>
2025-02-17 17:20:46 +0300

    update user serializers

--- kerala/users/serializers.py
+++ kerala/users/serializers.py
@@ -17,4 +17,12 @@ from .models import User
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["username", "user_type", "email"]
+        fields = ['id', "username", "user_type", "email"]
+
+from rest_framework import serializers
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "user_type"]
