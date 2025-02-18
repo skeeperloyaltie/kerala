0ca14e1c19fa99b872bbd839824e55c7a2c9f5b1
skeeperloyaltie <gugod254@gmail.com>
2025-02-18 14:42:19 +0300

    updated admin abd models  - chek that we can get user info correcty

--- kerala/users/admin.py
+++ kerala/users/admin.py
@@ -7,10 +7,31 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'first_name','last_name', 'user_type', 'is_active')
     search_fields = ('username', 'email')
 
-@admin.register(Receptionist)
-class ReceptionistAdmin(admin.ModelAdmin):
-    list_display = ('user', 'first_name','last_name', 'contact_number', 'email')
+from django.contrib import admin
+from .models import Doctor, Receptionist
 
-@admin.register(Doctor)
 class DoctorAdmin(admin.ModelAdmin):
-    list_display = ('user', 'first_name','last_name',  'specialization', 'contact_number', 'email')
+    list_display = ('user', 'get_first_name', 'get_last_name', 'specialization', 'contact_number', 'email')
+
+    @admin.display(ordering='user__first_name', description='First Name')
+    def get_first_name(self, obj):
+        return obj.user.first_name
+
+    @admin.display(ordering='user__last_name', description='Last Name')
+    def get_last_name(self, obj):
+        return obj.user.last_name
+
+class ReceptionistAdmin(admin.ModelAdmin):
+    list_display = ('user', 'get_first_name', 'get_last_name', 'contact_number', 'email')
+
+    @admin.display(ordering='user__first_name', description='First Name')
+    def get_first_name(self, obj):
+        return obj.user.first_name
+
+    @admin.display(ordering='user__last_name', description='Last Name')
+    def get_last_name(self, obj):
+        return obj.user.last_name
+
+admin.site.register(Doctor, DoctorAdmin)
+admin.site.register(Receptionist, ReceptionistAdmin)
+
