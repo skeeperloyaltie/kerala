eeb10cfe87ff0a4d55512eabbf1a131d6c7e95d6
skeeperloyaltie <gugod254@gmail.com>
2025-03-07 17:48:39 +0300

    fix endpoint

--- kerala/patients/urls.py
+++ kerala/patients/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import PatientListView
 
 urlpatterns = [
-    path("patients/list/", PatientListView.as_view(), name="patient-list"),
+    path("list/", PatientListView.as_view(), name="patient-list"),
 ]
