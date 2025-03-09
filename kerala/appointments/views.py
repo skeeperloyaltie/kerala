096806004e4375d978f69e5226aacca3b6c0b301
skeeperloyaltie <gugod254@gmail.com>
2025-03-09 03:58:54 +0300

    doctor representation call

--- kerala/appointments/views.py
+++ kerala/appointments/views.py
@@ -41,7 +41,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from .models import Appointment, Patient, Doctor
 from .serializers import AppointmentSerializer
 from datetime import datetime
 import pytz
