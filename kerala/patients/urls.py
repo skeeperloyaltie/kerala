4fdf4ccbfc198be72598f5891f343315619e6142
skeeperloyaltie <gugod254@gmail.com>
2025-03-31 04:46:13 +0300

    from details to detail

--- kerala/patients/urls.py
+++ kerala/patients/urls.py
@@ -4,5 +4,5 @@ from .views import PatientListView, PatientSearchView, PatientDetailView
 urlpatterns = [
     path("list/", PatientListView.as_view(), name="patient-list"),
     path('search/', PatientSearchView.as_view(), name='patient-search'),
-    path('details/<int:pk>/', PatientDetailView.as_view(), name='patient-detail'),
+    path('detail/<int:pk>/', PatientDetailView.as_view(), name='patient-detail'),
 ]
